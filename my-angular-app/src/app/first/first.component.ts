08d70879da703646a9e5e2c3b679a2e4c8a0d3f3
Mario Lazzari <mario.lazzari@gmail.com>
2020-05-07 20:09:26 +0200

    Done lesson 3

--- my-angular-app/src/app/first/first.component.ts
+++ my-angular-app/src/app/first/first.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponent implements OnInit {
   text: string = 'My first component is ready.';
+  numbers: number[] = [1, 2, 3, 4, 5];
+  isGreen: boolean = false;
+  date: Date = new Date();
 
   constructor() {}
 
